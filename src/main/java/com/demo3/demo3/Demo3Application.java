e2a6a98a1e2ea3369f51956ac98f81294be17de5
Daniel Peedah <daniel.peedah@netbuilder.com>
2021-05-05 14:23:37 +0100

    First pass

--- src/main/java/com/demo3/demo3/Demo3Application.java
+++ src/main/java/com/demo3/demo3/Demo3Application.java
@@ -2,12 +2,21 @@ package com.demo3.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Demo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
 	}
 
+	@GetMapping
+	public List<> hello(){
+		return List.of("hello", "my", "name", "is", "daniel");
+	}
 }
