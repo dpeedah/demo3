2d2fa6dc44aa07bf3729877f676d0ff0b3bb65bc
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-10 12:54:55 +0100

    ACTOR: Changed testing method to use .equals, added tests for getters

--- src/test/java/com/demo3/demo3/actor/ActorClassTests.java
+++ src/test/java/com/demo3/demo3/actor/ActorClassTests.java
@@ -2,21 +2,54 @@ package com.demo3.demo3.actor;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ActorClassTests {
+
+//    @Test
+//    public void testGetId(){
+//        Actor actor = new Actor("first","last");
+//        Long test = actor.getId();
+//        assertTrue(test.equals(1L));
+//    }
+
+//    @Test
+//    public void testGetLastUpdate(){
+//        Actor actor = new Actor("first","last");
+//        Date test = actor.getLastUpdate();
+//        assertTrue(test.equals(CURRENTDATEHERE?));
+//    }
+
+    @Test
+    public void testGetFirstName(){
+        Actor actor = new Actor("first","last");
+        String test = actor.getFirstName();
+        assertTrue(test.equals("first"));
+    }
+
+    @Test
+    public void testGetLastName(){
+        Actor actor = new Actor("first","last");
+        String test = actor.getLastName();
+        assertTrue(test.equals("last"));
+    }
+
     @Test
     public void testSetLastName() {
         Actor actor = new Actor();
         actor.setLastName("test");
-        assertTrue(actor.getLastName() == "test");
+        String test = actor.getLastName();
+        assertTrue(test.equals("test"));
     }
 
     @Test
     public void testSetFirstName() {
         Actor actor = new Actor();
         actor.setFirstName("test");
-        assertTrue(actor.getFirstName() == "test");
+        String test = actor.getFirstName();
+        assertTrue(test.equals("test"));
     }
 
 }
