58e7663ef373de58208290eede2dc9088bef8328
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-11 12:47:27 +0100

    Progress on ActorClassTests

--- src/main/java/com/demo3/demo3/actor/ActorRepository.java
+++ src/main/java/com/demo3/demo3/actor/ActorRepository.java
@@ -1,6 +1,8 @@
 package com.demo3.demo3.actor;
 
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
 import java.util.Optional;
 public interface ActorRepository extends CrudRepository<Actor,Long> {
 
