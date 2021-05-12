06a4d50f1dde466c78e75adcc293e977ad0d5132
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-12 14:34:55 +0100

    fixed bug in a test

--- src/test/java/com/demo3/demo3/actor/ActorControllerTests.java
+++ src/test/java/com/demo3/demo3/actor/ActorControllerTests.java
@@ -2,10 +2,15 @@ package com.demo3.demo3.actor;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,5 +36,29 @@ class ActorControllerTests {
         actor.setLastName(lastName);
     }
 
+    @Test
+    public void testGetActors(){
+        Iterable<Actor> actors = actorController.getActors();
+        assertNotNull(actors);
+    }
+
+    @Test
+    public void testFindActorByIdValid(){
+        Optional<Actor> actor = actorController.findActorById(1L);
+        assertFalse(actor.isEmpty());
+    }
+
+    @Test
+    public void testFindActorByIdInvalid(){
+        Optional<Actor> actor = actorController.findActorById(101010101L);
+        assertTrue(actor.isEmpty());
+    }
+
+//    @Test
+//    public void testDeleteActor(){
+//        actorController.deleteActor(209L);
+//        Optional<Actor> actor = actorController.findActorById(209L);
+//        assertTrue(actor.isEmpty());
+//    }
 
 }
