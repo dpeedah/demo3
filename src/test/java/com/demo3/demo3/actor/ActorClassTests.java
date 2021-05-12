9389aa9e2d65dd1d4fbb0196b6f6dfead53962f9
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-12 16:52:15 +0100

    Actor and Category Class and Repo Tests working, now starting on controller tests

--- src/test/java/com/demo3/demo3/actor/ActorClassTests.java
+++ src/test/java/com/demo3/demo3/actor/ActorClassTests.java
@@ -1,17 +1,13 @@
 package com.demo3.demo3.actor;
 
-import com.demo3.demo3.film.Film;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
-import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
--- src/test/java/com/demo3/demo3/category/CategoryRepoTests.java
+++ src/test/java/com/demo3/demo3/category/CategoryRepoTests.java
@@ -1,7 +1,5 @@
 package com.demo3.demo3.category;
 
-import com.demo3.demo3.actor.Actor;
-import com.demo3.demo3.actor.ActorRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.Optional;
 
@@ -46,7 +46,7 @@ class CategoryRepoTests {
 
     @Test
     public void testGetLastUpdate(){
-        Optional<Category> category = categoryRepo.findById(1L);
+        Optional<Category> category = categoryRepo.findById(2L);
         Date categoryDate = category.get().getLastUpdate();
         Date testDate = Date.from(LocalDateTime.of(2006,02,15,04,46,27,0).toInstant(ZoneOffset.UTC));
         assertEquals(testDate.compareTo(categoryDate),0);
@@ -54,8 +54,8 @@ class CategoryRepoTests {
 
     @Test
     public void findCategoryByName(){
-        Optional<Category> category = categoryRepo.findCategoryByName("Action");
-        assertEquals("Action",category.get().getName());
+        Optional<Category> category = categoryRepo.findCategoryByName("Comedy");
+        assertEquals("Comedy",category.get().getName());
     }
 
     @Test
@@ -63,4 +63,26 @@ class CategoryRepoTests {
         Optional<Category> category = categoryRepo.findCategoryByName("Nonexistent");
         assertTrue(category.isEmpty());
     }
+
+    @Test
+    public void testCreateCategory(){
+        Category category = new Category();
+        category.setName("Anime");
+        Category savedCategory = categoryRepo.save(category);
+        assertNotNull(categoryRepo.findById(savedCategory.getCategoryId()));
+    }
+
+    //will test the auto generated lastupdated variable
+    @Test
+    public void testUpdateCategory(){
+        Category category = categoryRepo.findById(1L).get();
+        LocalDateTime ldtNow = LocalDateTime.now();
+        ZonedDateTime zdt = ldtNow.atZone(ZoneId.from(ZoneOffset.UTC));
+        Date testDatenow = Date.from(zdt.toInstant());
+        category.setName("Example");
+        categoryRepo.save(category);
+        category = categoryRepo.findById(1L).get();
+        assertEquals(testDatenow.compareTo(category.getLastUpdate()),1);
+    }
+
 }
