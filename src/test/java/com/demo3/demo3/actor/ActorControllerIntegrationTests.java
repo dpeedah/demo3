a109406acd5f5933b585de8cf15a0845ee41b2f5
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-14 11:54:53 +0100

    Pushing

--- src/test/java/com/demo3/demo3/actor/ActorControllerIntegrationTests.java
+++ src/test/java/com/demo3/demo3/actor/ActorControllerIntegrationTests.java
@@ -2,11 +2,13 @@ package com.demo3.demo3.actor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,10 +17,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class ActorControllerIntegrationTests {
+public class ActorControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
---
+++ src/test/resources/ActorCrud.feature
@@ -0,0 +1,4 @@
+Feature: List of actors retrieved
+  Scenario : user makes a call to GET /apis
+    When user calls /apis
+    Then user receives error of code 200
