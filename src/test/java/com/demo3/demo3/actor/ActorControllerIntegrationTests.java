18f1eb22428b41e766b22f5067f30949fffc4425
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-18 09:38:34 +0100

    Made Actor Tests not use Jupiter to test if they run in jenkins

--- src/test/java/com/demo3/demo3/actor/ActorControllerIntegrationTests.java
+++ src/test/java/com/demo3/demo3/actor/ActorControllerIntegrationTests.java
@@ -1,20 +1,20 @@
 package com.demo3.demo3.actor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ActorControllerIntegrationTests {
